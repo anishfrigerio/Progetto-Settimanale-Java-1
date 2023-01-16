7cd7f8c4c8ef22fae79f78d1757962444d1ed043
anishfrigerio <114395195+anishfrigerio@users.noreply.github.com>
2023-01-16 11:57:28 +0100

    Update Video.java

--- ProgettoPlayerMultimediale/src/progettoSettimanale/Video.java
+++ ProgettoPlayerMultimediale/src/progettoSettimanale/Video.java
@@ -56,7 +56,7 @@ public class Video extends ElementoMultimediale implements ElementoRiproducibile
 	@Override
 	public void play() {
 		// TODO Auto-generated method stub
-		System.out.println(this.getTitle()+ this.getDurata() + dispVol);
+		System.out.println(this.getTitle()+ this.getDurata() + dispVol + "/n" + dispLum);
 		
 	}
 
